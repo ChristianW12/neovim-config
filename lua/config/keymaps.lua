133dfa89ec317d6b880e9c8040970be5a58079f6
Christian <christian.wagner1204@gmail.com>
2026-04-10 09:03:46 +0200

    adding keymap for removing highlights

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,12 +6,13 @@ local map = vim.keymap.set
 -- =========================
 -- Zum nächsten Buffer springen
 map("n", "L", ":bnext<CR>", { desc = "Nächster Buffer", silent = true })
-
 -- Zum vorherigen Buffer springen
 map("n", "H", ":bprevious<CR>", { desc = "Vorheriger Buffer", silent = true })
-
 -- Buffer schließen
 map("n", "<leader>x", ":bd<CR>", { desc = "Buffer schließen", silent = true })
+
+-- Highlights nach suche entfernen
+map("n", "<leader>nh", ":nohl<CR>", { desc = "Suche-Highlight entfernen", silent = true })
 
 -- =========================
 -- Obsidian Mappings (Globale Triggers)
@@ -19,12 +20,10 @@ map("n", "<leader>x", ":bd<CR>", { desc = "Buffer schließen", silent = true })
 -- Diese Mappings starten das Obsidian-Plugin bei Bedarf (Lazy Loading)
 map("n", "<leader>os", "<cmd>ObsidianSearch<cr>", { desc = "Obsidian: Suche Text" })
 map("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<cr>", { desc = "Obsidian: Schnellwechsel Notiz" })
-
 -- Erstellung und Verlinkung
 map("n", "<leader>on", "<cmd>ObsidianNew<cr>", { desc = "Obsidian: Neue Notiz" })
 map("v", "<leader>ol", "<cmd>ObsidianLink<cr>", { desc = "Obsidian: Link erstellen (Auswahl)" })
 map("v", "<leader>on", "<cmd>ObsidianNewFromLink<cr>", { desc = "Obsidian: Notiz aus Link erstellen" })
-
 -- Navigation und Daily Notes
 map("n", "<leader>od", "<cmd>ObsidianToday<cr>", { desc = "Obsidian: Heute (Daily Note)" })
 map("n", "<leader>ot", "<cmd>ObsidianTags<cr>", { desc = "Obsidian: Suche Tags" })
